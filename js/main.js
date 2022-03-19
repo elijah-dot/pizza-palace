0967ddf5e18803a5cfbf50d8fdf03f4c2c921a30
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-03-19 09:16:25 +0300

    styly:styled section four and added features:hover and overlay effect

--- js/main.js
+++ js/main.js
@@ -28,3 +28,36 @@ $("#para3").click(function () {
 });
 
 // section four
+
+
+
+$(".boxfour").hover(
+  function () {
+    $(".topone").css("display", "block");
+    $(".boxfour").animate({ bottom: "10px" });
+  },
+  function () {
+    $(".topone").css("display", "none");
+    $(".boxfour").animate({ bottom: "0px" });
+  }
+);
+$(".boxfive").hover(
+  function () {
+    $(".toptwo").css("display", "block");
+    $(".boxfive").animate({ bottom: "10px" });
+  },
+  function () {
+    $(".toptwo").css("display", "none");
+    $(".boxfive").animate({ bottom: "0px" });
+  }
+);
+$(".boxsix").hover(
+  function () {
+    $(".topthree").css("display", "block");
+    $(".boxsix").animate({ bottom: "10px" });
+  },
+  function () {
+    $(".topthree").css("display", "none");
+    $(".box").animate({ bottom: "0px" });
+  }
+);
