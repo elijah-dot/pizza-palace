45696aa43b4c8cdfe024684bd0d67b8a56510d44
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-03-19 07:40:21 +0300

    js:fit:added a click feature in boxone

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-// efects
+// efects// section three
+
+
+$("#ab").click(function () {
+    $("#para1").css("display", "block");
+    $("#ab").css("display", "none");
+  });
+  $("#para1").click(function () {
+    $("#ab").css("display", "block");
+    $("#para1").css("display", "none");
+  });
+  
