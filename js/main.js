ecbb89fb0b4ce64f6b508163ab0841c39fb8529f
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-03-19 07:44:28 +0300

    js:fit:added a click feature in boxthree

--- js/main.js
+++ js/main.js
@@ -10,10 +10,19 @@ $("#para1").click(function () {
 });
 
 $("#cd").click(function () {
-    $("#para2").css("display", "block");
-    $("#cd").css("display", "none");
-  });
-  $("#para2").click(function () {
-    $("#cd").css("display", "block");
-    $("#para2").css("display", "none");
-  });
+  $("#para2").css("display", "block");
+  $("#cd").css("display", "none");
+});
+$("#para2").click(function () {
+  $("#cd").css("display", "block");
+  $("#para2").css("display", "none");
+});
+
+$("#ef").click(function () {
+  $("#para3").css("display", "block");
+  $("#ef").css("display", "none");
+});
+$("#para3").click(function () {
+  $("#ef").css("display", "block");
+  $("#para3").css("display", "none");
+});
