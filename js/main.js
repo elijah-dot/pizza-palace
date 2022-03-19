cf8f4e249193831713b9be253dd863e037ecd7a9
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-03-19 07:42:45 +0300

    js:fit:added a click feature in boxtwo

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 // efects// section three
 
-
 $("#ab").click(function () {
-    $("#para1").css("display", "block");
-    $("#ab").css("display", "none");
+  $("#para1").css("display", "block");
+  $("#ab").css("display", "none");
+});
+$("#para1").click(function () {
+  $("#ab").css("display", "block");
+  $("#para1").css("display", "none");
+});
+
+$("#cd").click(function () {
+    $("#para2").css("display", "block");
+    $("#cd").css("display", "none");
   });
-  $("#para1").click(function () {
-    $("#ab").css("display", "block");
-    $("#para1").css("display", "none");
+  $("#para2").click(function () {
+    $("#cd").css("display", "block");
+    $("#para2").css("display", "none");
   });
-  
